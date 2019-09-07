bf1dece50d47a2219d6ce06c47e6d70da644a3f5
tu-nguyen <valcrune@gmail.com>
2019-09-07 10:46:14 -0400

    added js template and first kata

---
+++ javascript/Multiply.js
@@ -0,0 +1,32 @@
+// Title: Multiply
+// Rank: 8 kyu
+// Language Version: JavaScript Node v8.1.3
+
+// Instructions //
+//The code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+
+// Solution //
+function multiply(a, b) {
+    return a * b
+}
+
+// Test Case //
+// TODO: Add your tests here
+// Starting from Node 10.x, [Mocha](https://mochajs.org) is used instead of our custom test framework.
+// [Codewars' assertion methods](https://github.com/Codewars/codewars.com/wiki/Codewars-JavaScript-Test-Framework)
+// are still available for now.
+//
+// For new tests, using [Chai](https://chaijs.com/) is recommended.
+// You can use it by requiring:
+//     const assert = require("chai").assert;
+// If the failure output for deep equality is truncated, `chai.config.truncateThreshold` can be adjusted.
+
+describe("Solution", function () {
+    it("should test for something", function () {
+        // Test.assertEquals(1 + 1, 2);
+        // assert.strictEqual(1 + 1, 2);
+    });
+});
