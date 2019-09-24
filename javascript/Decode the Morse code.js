06cc5190a8d417f2dbe14cbfb11c855b51b114c3
tu-nguyen <valcrune@gmail.com>
2019-09-24 19:46:20 -0400

    started js 6 kyu Decode the Morse code

---
+++ javascript/Decode the Morse code.js
@@ -0,0 +1,50 @@
+// Title: Decode the Morse code
+// Rank: 6 kyu
+// Language Version: JavaScript Node v8.1.3
+
+// Instructions //
+// ...
+// Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+// For example:
+// decodeMorse('.... . -.--   .--- ..- -.. .')
+// //should return "HEY JUDE"
+// ...
+//
+// Solution //
+decodeMorse = function(morseCode){
+  code = morseCode.split(" ");
+  
+  console.log(morseCode);
+  console.log(morseCode.length);
+  
+  var i;
+  var res = "";
+  for (i = 0; i < code.length; i++) {
+    if (code[i] == '') {// && code[i + 1] == '') {
+      res = res.concat(" ");
+      while (code[i + 1] == '') {
+        i++;
+      }
+//       i++;
+    }
+//     if (code[i] == '') {
+//       while (code[i + 1]) {
+//         res = res.concat(" ");
+//         i++
+//       }
+//     }
+    else {
+      res = res.concat(MORSE_CODE[code[i]]);
+    }
+  }
+  return res;
+}
+
+// Test Case //
+Test.describe("Example from description", function(){
+Test.assertEquals(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
+});
+
+Test.describe("Your own tests", function(){
+// Add more tests here
+});
