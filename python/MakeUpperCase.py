9d1c751320f743f1ad2f595ca8140cc87ac19bc5
Tu Nguyen <valcrune@gmail.com>
2019-10-16 12:20:16 -0400

    completed python 8 kyu makeuppercase

---
+++ python/MakeUpperCase.py
@@ -0,0 +1,13 @@
+# Title: MakeUpperCase
+# Rank: 8 kyu
+# Language Version: Python 3.4.3
+
+## Instructions ##
+# Write function makeUpperCase.
+#
+## Solution ##
+def make_upper_case(s):
+    return s.upper()
+
+## Test Case ##
+test.assert_equals(make_upper_case("hello"), "HELLO")
